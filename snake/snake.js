53f83fe573ed5269b481e53ffca086d5eb459a01
cute apple <hidden>
2018-12-25 13:25:07 +0800

    ipnut and basics
    RegisterGlobalArrowKeyHandler
    basic data

--- snake/snake.js
+++ snake/snake.js
@@ -1,71 +1,64 @@
-﻿let playground
-let body = []
-let foods = []
-let controller = new controller4()
-let velocity = [1, 0]
-let width = 16
-let height = 16
+﻿let game = new Vue({
+    el: '#app',
+    data: {
+        hint: '🐍snake🐍😵🍱🎉',
+        
+        /**
+         * [ [x,y] , [x2,y2] , ... ]
+         */
+        body: [[0, 0]],
+        moveIntent: [0, 0],
+        end = false
+    },
+    computed: {},
+    methods: {
+        move(dx, dy) {}
+    },
+    created() {
+        
+    },
+    mounted() {
+        let { up, down, left, right } = this.movement;
+        RegisterGlobalArrowKeyHandler(
+            () => this.moveIntent = up,//up
+            () => this.moveIntent = down,//down
+            () => this.moveIntent = left,//left
+            () => this.moveIntent = right//right
+        )
+    }
+})
 
-let auto = true;
-let auto_int = 100;
-let auto_timer;
-
-let playground_util = {
-    in: (x, y) => x >= 0 && x < width && y >= 0 && y < height
+/**
+ * register a set of global arrow key event handler
+ * any undefined handler would not cause error
+ * @param {()=>void} up handler for up key down event
+ * @param {()=>void} down handler for down key down event
+ * @param {()=>void} left handler for left key down event
+ * @param {()=>void} right handler for right key down event
+ */
+function RegisterGlobalArrowKeyHandler(up, down, left, right) {
+    //cuteapple@github 2018
+    document.addEventListener('keydown', handler)
+    function handler(ev) {
+        let target;
+        switch (ev.key) {
+            case "ArrowLeft": target = left; break;
+            case "ArrowRight": target = right; break;
+            case "ArrowUp": target = up; break;
+            case "ArrowDown": target = down; break;
+            default: break; // do nothing
+        }
+        if (target)
+            target();
+    }
 }
 
-function init() {
-    playground = document.getElementById('playground')
-    playground.style.gridTemplateColumns = `repeat(${width},1fr)`;
-    playground.style.gridTemplateRows = `repeat(${height},1fr)`;
-    body = [new SnakeBody(1, 0, true), new SnakeBody(0, 0), new SnakeBody(0, 1), new SnakeBody(0, 2)]
-    foods = [new Food(...randomEmptyGrid())]
-    controller.all = (...args) => {
-        movekey_handler(...args);
-        auto_timer = setInterval(() => move(...velocity), auto_int);
-        controller.all = movekey_handler;
-    }
-
-}
-
-function move(dx, dy) {
-    let [x, y] = body[0].pos
-    x += dx
-    y += dy
-
-    let status;
-
-    //wall
-    if (!playground_util.in(x, y)) { body[0].blink(); end('😵'); return; }
-    //food
-    let eat_food = foods.find(food => food.x == x && food.y == y)
-    let eat_full = false;
-    if (eat_food) {
-        let emptyPos = randomEmptyGrid();
-        if (emptyPos) {
-            eat_food.pos = emptyPos;
-        }
-        else {
-            eat_food.detech()
-            eat_full = true;
-        }
-        body.push(new SnakeBody(0, 0))//pos not relevent
-    }
-
-    for (let i = body.length - 1; i > 0; --i) {
-        body[i].pos = body[i - 1].pos
-    }
-    body[0].pos = [x, y]
-
-    if (eat_full) end('🍱');
-
-    let head = body[0]
-    let self_eat = body.find(b => b !== head && b.x == head.x && b.y == head.y)
-    if (self_eat) {
-        head.blink();
-        self_eat.blink();
-        end('😵');
-    }
+/**
+ * return int between [0,upper)
+ * @param {Number} upper
+ */
+function randomInt(upper) {
+    return Math.floor(Math.random() * upper)
 }
 
 class PlaygroundItem {
@@ -136,24 +129,4 @@ function randomEmptyGrid() {
             }
         }
     }
-}
-
-function hint(text) {
-    document.getElementById('hint').innerText = text;
-}
-
-function end(message = "end") {
-    controller.all = () => { }
-    clearInterval(auto_timer)
-    hint(message)
-}
-
-function movekey_handler(direction) {
-    let v = controller4.to2D(direction, [1, -1])
-    if (body.length > 1) {
-        let ov = [body[0].x - body[1].x, body[0].y - body[1].y]
-        if (v[0] == -ov[0] && v[1] == -ov[1]) return;//invalid move
-    }
-    velocity = v;
-    if (!auto) move(...velocity)
 }
