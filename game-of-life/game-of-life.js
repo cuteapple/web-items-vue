a123d4c55809acac125ce0cbed3b658777593b00
cute apple <hidden>
2018-12-26 15:56:31 +0800

    copy GameOfLife class from origin repo

--- game-of-life/game-of-life.js
+++ game-of-life/game-of-life.js
@@ -49,3 +49,58 @@ const game = new Vue({
     el: '#app',
     data: { width: 10, height: 10, scale: 5 }
 })
+
+
+class GameOfLife {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+        this.initGrid()
+    }
+
+    initGrid() {
+        this.grid = Array(this.width).fill().map(x => []);
+        for (let w = 0; w < this.width; ++w) {
+            for (let h = 0; h < this.height; ++h) {
+                this.grid[w][h] = Math.random() > 0.5 ? 1 : 0;
+            }
+        }
+    }
+
+    //rewrite this to produce loop effect or something...
+    sample(w, h) {
+        //clamp to edge = 0
+        let a = this.grid[w]
+        let b = a == undefined ? undefined : a[h]
+        return b == undefined ? 0 : b
+    }
+
+    //compute next iteration value of certain grid
+    computeNext(w, h) {
+        let sum = 0
+        for (let dw of [-1, 0, 1])
+            for (let dh of [-1, 0, 1])
+                sum += this.sample(w + dw, h + dh)
+        sum -= this.sample(w, h)
+        switch (this.sample(w, h)) {
+            case 0:
+                return sum == 3 ? 1 : 0
+                break;
+            case 1:
+                return sum < 2 ? 0 :
+                    sum <= 3 ? 1 :
+                        0
+                break;
+        }
+    }
+
+    nextEpoch() {
+        let newGrid = Array(this.width).fill().map(x => []);
+        for (let w = 0; w < this.width; ++w) {
+            for (let h = 0; h < this.height; ++h) {
+                newGrid[w][h] = this.computeNext(w, h)
+            }
+        }
+        this.grid = newGrid;
+    }
+}
